2e9572caa870b5bb3f9c5ce1a72a12ff1501d3bf
Zahyr Seferina <zahyr.seferina@gmail.com>
2024-07-09 14:23:44 +0200

    chore: add autoprefixer for browser support

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -15,6 +15,9 @@ const config: Config = {
       },
     },
   },
-  plugins: [require('@tailwindcss/forms'),],
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('autoprefixer')
+  ],
 };
 export default config;
